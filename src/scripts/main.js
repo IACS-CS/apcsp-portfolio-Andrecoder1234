7c301febddb928a2128aacb05874eb93f94f4651
Andrecoder1234 <rocketpirate33@gmail.com>
2024-10-03 13:24:14 +0000

    steps 1-3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,21 @@
 import "../styles/styles.css";
 // Add imports here
-
+import Aos from "aos";
+import "aos/dist/aos.css";  
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
-  //
+  console.log("Hello world, from main.js!");
+  Aos.init();
+
+
+
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
+import "@picocss/pico/css/pico.min.css";
+
+
+
