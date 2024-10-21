5ccd66da7772d5422ffd7d67b0101dc274fa4223
Andrecoder1234 <rocketpirate33@gmail.com>
2024-10-21 15:47:48 +0000

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,18 @@ import "../styles/styles.css";
 // Add imports here
 import Aos from "aos";
 import "aos/dist/aos.css";  
+import 'animate.css';
+// This project uses Animate.css
+// License: MIT License
+// Copyright (c) 2014 Daniel Eden
+// Full license text in LICENSE file.
+
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
+  
   console.log("Hello world, from main.js!");
   Aos.init();
 
@@ -15,6 +22,6 @@ if (typeof window !== "undefined") {
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
-import "@picocss/pico/css/pico.min.css";
 
+import "@picocss/pico/css/pico.min.css";
 
